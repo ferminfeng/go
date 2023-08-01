914b58c8b9abb5a9fa433cdfadf2315ec1233743
fermin <fuyifeng@51k1k.com>
2023-08-01 11:46:34 +0800

    grpc

--- grpc/order/server/greeter.go
+++ grpc/order/server/greeter.go
@@ -9,8 +9,9 @@ import (
 type GreeterServer struct {
 }
 
+// SayHello ...
 func (s *GreeterServer) SayHello(ctx context.Context, req *wrappers.StringValue) (resp *wrappers.StringValue, err error) {
 	resp = &wrappers.StringValue{}
-	log.Printf("[greeter server]Hello, %s, this is greeter server.", req.Value)
+	log.Printf("[问候服务]Hello, %s, this is greeter server.", req.Value)
 	return
 }
